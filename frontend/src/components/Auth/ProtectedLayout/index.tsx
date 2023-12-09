1328c0bb224db82808ae2c12e21b4d5b3241abea
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2023-12-09 16:37:46 -0300

    feat: improve react router behavior at index page

--- frontend/src/components/Auth/ProtectedLayout/index.tsx
+++ frontend/src/components/Auth/ProtectedLayout/index.tsx
@@ -1,14 +1,19 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../../../context/AuthContext";
 import { Layout } from "../../Layout";
 
 
 export const ProtectedLayout = () => {
   const { isAuthenticated } = useAuth();
+  const location = useLocation();
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" replace />;
   }
+
+  if (isAuthenticated && location.pathname === "/") {
+    return <Navigate to="/dashboard" replace />;
+  } 
 
   return (
     <Layout>
